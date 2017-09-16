ddd85dab9aa6f20eb898ddd3767f45089b48cca5
Liang Yuan <superylbyr@gmail.com>
2017-09-16 00:15:39 -0700

    Main page design

---
+++ src/components/Search.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="search">
+    <div class="title row">
+      <el-row type="flex" class="row-bg" justify="center">
+        <el-col :span="6"><div class="grid-content">
+          <div>
+            <div class="title-component" id="title-image">
+              <img src="../assets/ic_shopping_cart_large.svg">
+            </div>
+            <div class="title-component" id="title-text">
+              <h1>Ads Search</h1>
+            </div>
+          </div>
+        </div></el-col>
+      </el-row>
+    </div>
+    <div class="search row">
+      <el-row>
+        <el-col :span="12" :offset="6">
+          <div class="grid-content">
+            <el-input autofocus size="large" v-model="searchInput">
+              <el-button slot="append" icon="search"></el-button>
+            </el-input>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="repository row">
+      <a href="https://github.com/herospYL/ads-search-vue">
+        <img src="../assets/GitHub-Mark-32px.png">
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'search',
+    data() {
+      return {
+        msg: 'Welcome to Ads Search Vue.js App',
+        searchInput: '',
+      };
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  * {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
+
+  h1, h2 {
+    font-weight: bold;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  .row {
+    margin-top: 2%;
+    margin-bottom: 2%;
+  }
+
+  .title-component {
+    float:left;
+  }
+
+  #title-image{
+    margin-left:10%;
+  }
+
+  #title-text{
+    margin-left:5%;
+  }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
+import locale from 'element-ui/lib/locale/lang/en';
 import router from './router';
+import App from './App';
 
-Vue.config.productionTip = false;
+Vue.use(ElementUI, { locale });
+
+Vue.config.productionTip = false; // Turn on when release
 
 /* eslint-disable no-new */
 new Vue({
